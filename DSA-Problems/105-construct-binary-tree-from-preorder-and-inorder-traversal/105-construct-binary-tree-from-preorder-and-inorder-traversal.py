f4a84fc4f2d0b1e377bc0c018e4e751e79e98a80
shubh-2006 <shubhagarwal206@gmail.com>
2026-01-07 11:16:22 +0530

    Update 105-construct-binary-tree-from-preorder-and-inorder-traversal.py

--- DSA-Problems/105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ DSA-Problems/105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,1 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+  def buildTree(
+      self,
+      preorder: list[int],
+      inorder: list[int],
+  ) -> TreeNode | None:
+    inToIndex = {num: i for i, num in enumerate(inorder)}
 
+    def build(
+        preStart: int,
+        preEnd: int,
+        inStart: int,
+        inEnd: int,
+    ) -> TreeNode | None:
+      if preStart > preEnd:
+        return None
+
+      rootVal = preorder[preStart]
+      rootInIndex = inToIndex[rootVal]
+      leftSize = rootInIndex - inStart
+
+      root = TreeNode(rootVal)
+      root.left = build(preStart + 1, preStart + leftSize,
+                        inStart, rootInIndex - 1)
+      root.right = build(preStart + leftSize + 1,
+                         preEnd, rootInIndex + 1, inEnd)
+      return root
+
+    return build(0, len(preorder) - 1, 0, len(inorder) - 1)
